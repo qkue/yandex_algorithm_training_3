a82b55dfc4255a2ee5056f8fcfe66c4d32a4b69c
qkue <130838079+qkue@users.noreply.github.com>
2024-05-16 12:13:56 +0300

    Create solution.py

---
+++ Division B/New tasks for past lecture topics/5/solution.py
@@ -0,0 +1,21 @@
+import string
+
+alphabet = string.ascii_lowercase
+letters = dict()
+
+n = int(input())
+for i in range(n):
+    letters[alphabet[i]] = int(input())
+alphabet = alphabet[:n]
+answer = 0
+
+for char in range(n - 1):
+    current_letter = letters[alphabet[char]]
+    next_letter = letters[alphabet[char + 1]]
+    if current_letter and next_letter:
+        if next_letter > current_letter:
+            answer += current_letter
+        elif next_letter <= current_letter:
+            answer += next_letter
+
+print(answer)
