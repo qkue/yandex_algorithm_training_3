109eca89d73fb30322c34a75b0e44eb86fb47148
qkue <130838079+qkue@users.noreply.github.com>
2024-05-17 21:21:47 +0300

    Update solution.py

--- Division B/New tasks for past lecture topics/8/solution.py
+++ Division B/New tasks for past lecture topics/8/solution.py
@@ -1,2 +1,12 @@
 # Python 3.12.1
 
+k = int(input())
+x = []
+y = []
+
+for _ in range(k):
+    left, right = map(int, input().split())
+    x.append(left)
+    y.append(right)
+
+print(min(x), min(y), max(x), max(y))
