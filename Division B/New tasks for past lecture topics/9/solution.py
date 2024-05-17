affc48edbd6c7bde633704dec0be5417f50652e0
qkue <130838079+qkue@users.noreply.github.com>
2024-05-17 21:21:18 +0300

    Update solution.py

--- Division B/New tasks for past lecture topics/9/solution.py
+++ Division B/New tasks for past lecture topics/9/solution.py
@@ -1,2 +1,27 @@
-# Python 3.12.1
+# Python 3.9 (PyPy 7.3.11)
 
+n, m, k = map(int, input().split())
+matrix = []
+prefixsum_row = []
+for i in range(n):
+    row = list(map(int, input().split()))
+    matrix.append(row)
+    prefixsum_row.append([0])
+    for elem in range(1, len(row) + 1):
+        prefixsum_row[i].append(prefixsum_row[i][elem-1] + row[elem - 1])
+
+answer = []
+for i in range(k):
+# for i in range(1):
+    x1, y1, x2, y2 = map(int, input().split()) # t.split()) 
+    if x1 > x2:
+        x1, x2 = x2, x1
+    if y1 > y2:
+        y1, y2 = y2, y1
+
+    current_res = 0
+    for j in range(x1 - 1, x2):
+        current_res += prefixsum_row[j][y2] - prefixsum_row[j][y1 - 1]
+    answer.append(str(current_res))
+
+print('\n'.join(answer))
