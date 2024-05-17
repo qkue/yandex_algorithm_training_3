23d688cf029c869fd1b1cf08f668d9a369ef1f7d
qkue <130838079+qkue@users.noreply.github.com>
2024-05-17 21:20:20 +0300

    Update solution.py

--- Division B/New tasks for past lecture topics/10/solution.py
+++ Division B/New tasks for past lecture topics/10/solution.py
@@ -1,2 +1,12 @@
 # Python 3.12.1
 
+text = input()
+char_dict = dict()
+n = len(text)
+
+for i in range(n):
+    letter = text[i]
+    char_dict[letter] = char_dict.get(letter, 0) + (i + 1) * (n - i)
+
+for i in sorted(char_dict):
+    print(f'{i}: {char_dict[i]}')
