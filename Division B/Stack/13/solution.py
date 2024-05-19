b81de92cabe765bf3cc9dbfcee920a81cea0a6f7
qkue <130838079+qkue@users.noreply.github.com>
2024-05-19 22:03:29 +0300

    Update solution.py

--- Division B/Stack/13/solution.py
+++ Division B/Stack/13/solution.py
@@ -1,2 +1,50 @@
 # Python 3.12.1
 
+class Stack:
+    def __init__(self):
+        self.array = []
+
+    def push(self, item):
+        self.array.append(item)
+
+    def pop(self):
+        if not self.size():
+            return None
+        return self.array.pop()
+
+    def size(self):
+        return True if self.array else False
+    
+    def back(self):
+        return self.array[-1]
+        
+
+arr = Stack()
+t = '8 7 +'
+text = input().split() #t.split()
+error = False
+
+def sign_oper(sign, left, right):
+    res = 0
+    if sign == '-':
+        res = left - right
+    elif sign == '+':
+        res = left + right
+    elif sign == '*':
+        res = left * right
+    return res
+
+for char in text:
+    if char in '+-*':
+        right = arr.pop()
+        left = arr.pop()
+        if right == None or left == None:
+            error = True
+            break
+        result = sign_oper(char, left, right)
+        arr.push(result)
+
+    else:
+        arr.push(int(char))
+
+print(arr.back())
