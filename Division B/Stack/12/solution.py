76c412afcbf4a905f64e8ff92f65abc06114dfa7
qkue <130838079+qkue@users.noreply.github.com>
2024-05-19 22:02:33 +0300

    Update solution.py

--- Division B/Stack/12/solution.py
+++ Division B/Stack/12/solution.py
@@ -1,2 +1,42 @@
 # Python 3.12.1
 
+class Stack:
+    def __init__(self):
+        self.array = []
+
+    def push(self, item):
+        self.array.append(item)
+
+    def pop(self, item):
+        if not self.size():
+            return False
+
+        if (self.back() == '(' and item == ')') or (self.back() == '[' and item == ']') or (self.back() == '{' and item == '}'):
+            last = self.array.pop()
+            return True
+        else:
+            return False
+
+    def size(self):
+        return True if self.array else False
+    
+    def back(self):
+        return self.array[-1]
+        
+
+arr = Stack()
+text = input()
+progress = True
+
+for char in text:
+    if char in ('([{'):
+        arr.push(char)
+    elif char in (')]}'):
+        progress = arr.pop(char)
+    if not progress:
+        break
+
+if progress and not arr.size():
+    print('yes')
+else:
+    print('no')
