c3e57e171b96943610cc46d1fbb6b011106810eb
qkue <130838079+qkue@users.noreply.github.com>
2024-05-29 12:55:37 +0300

    Update solution.py

--- Division B/Dynamic programming with one parameter/22/solution.py
+++ Division B/Dynamic programming with one parameter/22/solution.py
@@ -1,2 +1,16 @@
 # Python 3.12.1
 
+n, k = map(int, input().split())
+
+dp = [0] * (n + k)
+dp[0] = 1
+
+for i in range(1, k + 1):
+    for j in range(0, i):
+        dp[i] += dp[j]
+
+for i in range(k + 1, n + 1):
+    for j in range(i - k, i):
+        dp[i] += dp[j]
+
+print(dp[n - 1])
