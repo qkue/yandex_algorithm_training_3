a5cebf6fa91fcdd20e895e809a8a8610d18fa19d
qkue <130838079+qkue@users.noreply.github.com>
2024-05-29 09:41:33 +0300

    Update solution.py

--- Division B/Dynamic programming with one parameter/21/solution.py
+++ Division B/Dynamic programming with one parameter/21/solution.py
@@ -1,2 +1,29 @@
 # Python 3.12.1
+# Хороший разбор задания https://site.ada.edu.az/~medv/acm/Docs%20e-olimp/Volume%203/263.htm 
 
+n = int(input())
+
+num = [2, 4, 7]
+for i in range(3, n):
+    num.append(num[i - 1] + num[i - 2] + num[i - 3])
+print(num[n - 1])
+
+"""
+# Рекурсия с мемоизацией
+
+n = int(input())
+
+dp = [-1] * (n + 3)
+dp[1] = 2
+dp[2] = 4
+dp[3] = 7
+
+def func(n):
+    if dp[n] != -1: 
+        return dp[n]
+    
+    dp[n] = (func(n - 1) + func(n - 2) + func(n - 3))
+    return dp[n]
+
+print(func(n))
+"""
