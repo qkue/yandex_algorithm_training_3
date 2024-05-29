0b3c73c2cbf177820d243c506431da6e2b82e3fe
qkue <130838079+qkue@users.noreply.github.com>
2024-05-29 13:56:26 +0300

    Update solution.py

--- Division B/Dynamic programming with one parameter/22/solution.py
+++ Division B/Dynamic programming with one parameter/22/solution.py
@@ -1,5 +1,24 @@
 # Python 3.12.1
+# Разбор задания с получением формул https://site.ada.edu.az/~medv/acm/Docs%20e-olimp/Volume%2041/4051.htm
 
+n, k = map(int, input().split())
+
+dp = [0] * (30 + 1)
+dp[1] = 1
+dp[2] = 1
+
+for i in range(3, k + 1):
+    dp[i] = 2 * dp[i - 1]
+for i in range(k + 1, n + 1):
+    if i > 2:
+        dp[i] = 2 * dp[i - 1] - dp[i - k - 1]
+
+print(dp[n])
+
+
+
+# наивная реализация
+"""
 n, k = map(int, input().split())
 
 dp = [0] * (n + k)
@@ -14,3 +33,4 @@ for i in range(k + 1, n + 1):
         dp[i] += dp[j]
 
 print(dp[n - 1])
+"""
